90adf91eb578e44a0c006b5d46c5d74e8ecbea83
Javier Vivanco <javier@javier.Javier>
2026-01-17 22:20:38 -0500

    hola23

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import backgroundImage from '../assets/images/Tarjeta_Male.PNG';
 
 function Hero() {
   return (
-    <section id="hero" className="d-flex align-items-center text-center vh-100 bg-cover" style={{ backgroundImage: 'url(https://via.placeholder.com/1500x800?text=Casa+Limpia+%2B+Mascota)' }}>
+    <section id="hero" className="d-flex align-items-center text-center vh-100 bg-cover" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="container text-white">
         <h1 className="display-4 fw-bold">Cuidamos tu hogar y a tu mascota como si fueran nuestros</h1>
         <p className="lead my-4">Servicios profesionales de limpieza y cuidado de mascotas en tu ciudad</p>
